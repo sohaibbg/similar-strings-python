58ac398ec02ea18575ec914ad6e0cccc39ece099
Sohaib Baig <sohaibbg@gmail.com>
2022-08-07 00:37:17 +0500

    initial

--- filterView.dart
+++ filterView.dart
@@ -5,7 +5,7 @@ main() {
   File formatted = File('assets/routes.json');
   Map routes = jsonDecode(formatted.readAsStringSync());
   List<Map> filteredRoutes = [];
-  List<String> shouldContain = ['nipa', 'quaidabad'];
+  List<String> shouldContain = ['quaidabad'];
   routes.forEach((bus, stopList) {
     int matches = 0;
     stopList.forEach((stop) {
@@ -17,17 +17,18 @@ main() {
       filteredRoutes.add({bus: stopList});
     }
   });
-  for (int i = 0; i < filteredRoutes.length; i++) {
-    List stopList = filteredRoutes[i].values.first;
-    String key = filteredRoutes[i].keys.first;
-    int i1 =
-        stopList.indexWhere((stop) => stop.toLowerCase() == shouldContain[0]);
-    int i2 =
-        stopList.indexWhere((stop) => stop.toLowerCase() == shouldContain[1]);
-    if (i1 > i2) {
-      filteredRoutes[i][key] =
-          (filteredRoutes[i][key] as List).reversed.toList();
+  if (shouldContain.length > 1)
+    for (int i = 0; i < filteredRoutes.length; i++) {
+      List stopList = filteredRoutes[i].values.first;
+      String key = filteredRoutes[i].keys.first;
+      int i1 =
+          stopList.indexWhere((stop) => stop.toLowerCase() == shouldContain[0]);
+      int i2 =
+          stopList.indexWhere((stop) => stop.toLowerCase() == shouldContain[1]);
+      if (i1 > i2) {
+        filteredRoutes[i][key] =
+            (filteredRoutes[i][key] as List).reversed.toList();
+      }
     }
-  }
   File('assets/filterView.json').writeAsStringSync(jsonEncode(filteredRoutes));
 }
